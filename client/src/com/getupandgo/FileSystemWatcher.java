1a6e90434266ee1289aa984ef875708b64080fd6
getupandgo <getupandgo11@gmail.com>
2015-07-19 15:24:21 +0300

    initial commit

---
+++ client/src/com/getupandgo/FileSystemWatcher.java
@@ -0,0 +1,7 @@
+package com.getupandgo;
+
+/**
+ * Created by getupandgo on 7/19/15.
+ */
+public class FileSystemWatcher {
+}
---
+++ server/src/com/getupandgo/WebClientDriver.java
@@ -0,0 +1,8 @@
+package com.getupandgo;
+
+/**
+ * Created by getupandgo on 7/19/15.
+ */
+public class WebClientDriver {
+
+}
